5b6ae04437406ef967049e610124519cdc438318
youkwhd <lolywk@tutanota.com>
2024-05-10 05:58:04 +0700

    refactor: abstract Kitty type

--- kitty.nim
+++ kitty.nim
@@ -1,12 +1,14 @@
 import std/streams
 
-proc newKitty*(fpath: string): seq[string] =
+type Kitty* = seq[string]
+
+proc newKitty*(fpath: string): Kitty =
   let fstream: FileStream = newFileStream(fpath, fmRead)
   if isNil(fstream):
     quit(1)
 
   var line: string
-  var kitty: seq[string]
+  var kitty: Kitty
 
   while fstream.readLine(line):
     kitty.add(line)
--- pvfetch.nim
+++ pvfetch.nim
@@ -1,7 +1,7 @@
 import kitty
 
 proc main =
-  let kitty: seq[string] = newKitty("extra/kitty/1")
+  let kitty: Kitty = newKitty("extra/kitty/1")
 
   for line in kitty:
     echo line
