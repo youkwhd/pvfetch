f63b03115a891f4c55713bf643d070cb03e3854c
youkwhd <lolywk@tutanota.com>
2024-05-10 05:51:30 +0700

    chore: let it be 2 char wide indentation

--- kitty.nim
+++ kitty.nim
@@ -1,15 +1,15 @@
 import std/streams
 
-proc newKitty*(fpath: string): seq[string] = 
-    let fstream: FileStream = newFileStream(fpath, fmRead)
-    if isNil(fstream):
-        quit(1)
+proc newKitty*(fpath: string): seq[string] =
+  let fstream: FileStream = newFileStream(fpath, fmRead)
+  if isNil(fstream):
+    quit(1)
 
-    var line: string
-    var kitty: seq[string]
+  var line: string
+  var kitty: seq[string]
 
-    while fstream.readLine(line):
-        kitty.add(line)
+  while fstream.readLine(line):
+    kitty.add(line)
 
-    fstream.close()
-    return kitty
+  fstream.close()
+  return kitty
--- pvfetch.nim
+++ pvfetch.nim
@@ -1,10 +1,10 @@
 import kitty
 
-proc main = 
-    let kitty: seq[string] = newKitty("extra/kitty/1")
+proc main =
+  let kitty: seq[string] = newKitty("extra/kitty/1")
 
-    for line in kitty:
-        echo line
+  for line in kitty:
+    echo line
 
 when isMainModule:
-    main()
+  main()
