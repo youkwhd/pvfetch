36035f1f8ca772181c77d0017119aaf375ec158c
youkwhd <lolywk@tutanota.com>
2024-05-11 10:54:59 +0700

    feat: pretty print system info

--- pvfetch.nim
+++ pvfetch.nim
@@ -1,26 +1,34 @@
-import kitty, sysinfo
+import kitty, sysinfo, std/sequtils, extension/sequtils, std/strutils
+
+const MARGIN_RIGHT: uint = 2
 
 proc main =
   let kitty: Kitty = newKitty("extra/kitty/1")
   let longestLine = kitty.longestLine()
 
-  let username = getUsername()
-  let hostname = getHostname()
-  let kernelVersion = getKernelVersion()
-  let arch = getArchitecture()
-  let shell = getShell()
   let uptime = getUptime()
+  let sysinfo: seq[tuple[label, value: string]] = @[("OS", getOSName()), ("Kernel", getKernelVersion()), ("Shell", getShell()), ("Uptime", ($uptime.days & "d " & $uptime.hours & "h " & $uptime.minutes & "m"))]
+  let longestSysinfoLabel = sysinfo.reduce(proc (info: tuple[label, value: string], maximum: int): int = max(len(info.label), maximum), 0)
+  assert sysinfo.len >= kitty.len
 
-  echo username, "@", hostname
-  echo kernelVersion
-  echo arch
-  echo shell
-  echo uptime
-  
-  for line in kitty:
-    echo line
+  #[TODO(refactor):
+      - these prints can be capsulated into functions
+      - add colors]#
+  stdout.write repeat(" ", (longestLine + int(MARGIN_RIGHT))).join(), getUsername(), "@", getHostname(), "\n"
 
-  echo getOSName()
+  let n = sysinfo.len - kitty.len
+  for i in 0..n-1:
+    stdout.write repeat(" ", (longestLine + int(MARGIN_RIGHT))).join()
+    stdout.write sysinfo[i].label
+    stdout.write repeat(" ", (longestSysinfoLabel - len(sysinfo[i].label) + int(MARGIN_RIGHT))).join()
+    stdout.write sysinfo[i].value, "\n"
+
+  for i, line in kitty:
+    stdout.write line
+    stdout.write repeat(" ", (longestLine - len(line) + int(MARGIN_RIGHT))).join()
+    stdout.write sysinfo[i + n].label
+    stdout.write repeat(" ", (longestSysinfoLabel - len(sysinfo[i + n].label) + int(MARGIN_RIGHT))).join()
+    stdout.write sysinfo[i + n].value, "\n"
 
 when isMainModule:
   main()
