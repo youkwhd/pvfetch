ec103104fadd77efde34c80b68cc01cbfc6cf16b
youkwhd <lolywk@tutanota.com>
2024-05-14 17:37:25 +0700

    feat: add colors for sysinfo table

--- pvfetch.nim
+++ pvfetch.nim
@@ -1,4 +1,4 @@
-import kitty, sysinfo, extension/sequtils, std/strutils
+import kitty, ansi, sysinfo, extension/sequtils, std/strutils
 
 const MARGIN_RIGHT: uint = 2
 
@@ -13,34 +13,44 @@ proc main =
   if uptime.hours > 0: uptimev = $uptime.hours & "h "
   uptimev = $uptime.minutes & "m"
 
-  let sysinfo: seq[tuple[label, value: string]] = 
-    @[("OS", getOSName()),
-      ("Shell", getShell()),
-      ("Kernel", getKernelVersion()),
-      ("Uptime", uptimev)]
+  let sysinfo: seq[tuple[color, label, value: string]] = 
+    @[(YW, "OS", getOSName()),
+      (CN, "Shell", getShell()),
+      (BE, "Kernel", getKernelVersion()),
+      (PE, "Uptime", uptimev)]
 
   #[TODO(refactor):
       - these prints can be capsulated into functions
       - add colors]#
 
-  stdout.write repeat(" ", kitty.width + int(MARGIN_RIGHT)).join(), getUsername(), "@", getHostname(), "\n"
+  stdout.write repeat(" ", kitty.width + int(MARGIN_RIGHT)).join(),
+               RD,
+               getUsername(), "@", getHostname(),
+               RT,
+               "\n"
 
   assert sysinfo.len >= kittyArt.len
   let n = sysinfo.len - kittyArt.len
-  let longestSysinfoLabel = sysinfo.reduce(proc (info: tuple[label, value: string], maximum: int): int = max(len(info.label), maximum), 0)
+  let longestSysinfoLabel = sysinfo.reduce(proc (info: tuple[color, label, value: string], maximum: int): int = max(len(info.label), maximum), 0)
 
   for i in 0..n-1:
-    stdout.write repeat(" ", kitty.width + int(MARGIN_RIGHT)).join()
-    stdout.write sysinfo[i].label
-    stdout.write repeat(" ", (longestSysinfoLabel - len(sysinfo[i].label) + int(MARGIN_RIGHT))).join()
-    stdout.write sysinfo[i].value, "\n"
+    stdout.write repeat(" ", kitty.width + int(MARGIN_RIGHT)).join(),
+                 sysinfo[i].color,
+                 sysinfo[i].label,
+                 RT,
+                 repeat(" ", (longestSysinfoLabel - len(sysinfo[i].label) + int(MARGIN_RIGHT))).join(),
+                 sysinfo[i].value,
+                 "\n"
 
   for i, line in kittyArt:
-    stdout.write line
-    stdout.write repeat(" ", int(MARGIN_RIGHT)).join()
-    stdout.write sysinfo[i + n].label
-    stdout.write repeat(" ", (longestSysinfoLabel - len(sysinfo[i + n].label) + int(MARGIN_RIGHT))).join()
-    stdout.write sysinfo[i + n].value, "\n"
+    stdout.write line,
+                 repeat(" ", int(MARGIN_RIGHT)).join(),
+                 sysinfo[i + n].color,
+                 sysinfo[i + n].label,
+                 RT,
+                 repeat(" ", (longestSysinfoLabel - len(sysinfo[i + n].label) + int(MARGIN_RIGHT))).join(),
+                 sysinfo[i + n].value,
+                 "\n"
 
   stdout.write "\n"
 
